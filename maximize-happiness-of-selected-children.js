3e15e6ae8b7a7aee462bcf893fe3c9fb81172bdb
Nithin Kumar Balisetty <nithinkumarbalisetty98@gmail.com>
2025-12-24 23:08:08 -0600

    Easy - Greedy

---
+++ maximize-happiness-of-selected-children.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} happiness
+ * @param {number} k
+ * @return {number}
+ */
+var maximumHappinessSum = function(happiness, k) {
+    happiness.sort((a,b)=>(b-a));
+
+    let track = 0, ans = 0;
+
+    for(let i=0;i<k;i++){
+        ans += Math.max(happiness[i]-track,0)
+        track++;
+    }
+
+    return ans;
+};
